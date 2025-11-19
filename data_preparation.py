6c0ed0dee026000eec0e576fce1ec104c5d2f25a
Julia Neri <julia.neri@sheffield.ac.uk>
2025-11-19 17:14:19 +0000

    Add data cleaning steps

--- data_preparation.py
+++ data_preparation.py
@@ -6,5 +6,3 @@ print(df.head())
 from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
 df['species'] = le.fit_transform(df['species'])
-print("\nData after transformation:")
-print(df.head())
